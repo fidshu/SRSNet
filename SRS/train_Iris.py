ebbcec1cbbd70381047fc7c54116cf2fc33f9629
安达儿 <44111917+fidshu@users.noreply.github.com>
2025-08-24 12:32:01 +0800

    Update train_Iris.py

--- SRS/train_Iris.py
+++ SRS/train_Iris.py
@@ -1,7 +1,7 @@
 from trainer.trainer_Iris_SRS_R import trainer_SRS_R
 from trainer.trainer_Iris_SRS_S import trainer_SRS_S
 if __name__ == "__main__":
-#    trainer_SRS_R()
+    trainer_SRS_R()
     trainer_SRS_S()
     
     
